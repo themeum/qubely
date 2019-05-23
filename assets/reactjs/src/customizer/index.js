1837a2aec9feb166d333b05a1d64227f8bda5426
Anik Biswas <anikbiswas@Aniks-iMac.local>
2019-05-23 15:48:12 +0600

    Customizer Issue fixed

--- assets/reactjs/src/customizer/index.js
+++ assets/reactjs/src/customizer/index.js
@@ -1,9 +1,6 @@
-
-const { __ } = wp.i18n;
 const { registerPlugin } = wp.plugins;
 import Plugin from './plugin' 
 import icons from '../helpers/icons';
-
 
 registerPlugin( 'qubely-customizer', {
     icon: icons.qubely,
--- assets/reactjs/src/customizer/plugin.js
+++ assets/reactjs/src/customizer/plugin.js
@@ -7,8 +7,43 @@ import { _equal } from '../components/HelperFunction'
 import { CssGenerator } from '../components/CssGenerator'
 import { Typography, Color, Separator } from '../components/FieldRender'
 const { PluginSidebar, PluginSidebarMoreMenuItem } = wp.editPost;
-import { getInitialSettings, updateGlobalData, saveGlobalData } from './action'
 
+
+const path = { 
+    fetch: '/qubely/v1/global_settings',
+    post: '/qubely/v1/global_settings'
+}
+
+async function fetchFromApi() {
+    return await wp.apiFetch({path: path.fetch })
+}
+
+const getInitialSettings = () => {
+    return fetchFromApi().then( data => {
+        window.globalData = { ...data }
+        return data
+    } )
+}
+
+const updateGlobalData = ( updateData ) => {
+    const settings = {...window.globalData.settings}
+    window.globalData.settings = {...settings, ...updateData }
+}
+
+const saveGlobalData = () =>  {
+    window.globalSaving = true
+    const settings  = typeof window.globalData.settings === 'undefined' ? null : window.globalData.settings
+    if( settings !== null ){
+        wp.apiFetch({
+                path: path.post,
+                method: 'POST',
+                data: { settings: JSON.stringify(settings) }
+            }).then( data =>  {
+                window.globalSaving = false
+                return data 
+            })
+    }
+}
 
 class Plugin extends Component {
     constructor(){
@@ -164,3 +199,7 @@ wp.data.subscribe( () => {
         saveGlobalData()
     }
 } );
+
+// -----------------------------
+// -----------------------------
+// -----------------------------
