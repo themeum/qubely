a7b57c9f13de3347cfb4bba7cd71e4ad045cd95b
faisal29 <faisalcse29@gmail.com>
2019-06-18 13:16:57 +0600

    team social share gutter, pricing table feature custom alignment, border, padding , margin ,border radisu field update value

--- assets/reactjs/src/components/fields/Margin.js
+++ assets/reactjs/src/components/fields/Margin.js
@@ -39,7 +39,7 @@ class Margin extends Component {
 
         unit && value.unit ? newMargin.unit = value.unit : newMargin.unit = defaultUnit
         newMargin.marginType = value.marginType ? value.marginType : defaultMarginType
-        newMargin.openMargin = value.openMargin ? value.openMargin : 0
+        newMargin.openMargin = 1
         onChange(newMargin)
     }
     updateMarginType = (newType) => {
