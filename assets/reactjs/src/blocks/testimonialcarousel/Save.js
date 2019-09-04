415c7740ae43314dd9ebea9aa902c76b7726485a
faisal29 <faisalcse29@gmail.com>
2019-09-04 14:08:52 +0600

    completed layout three

--- assets/reactjs/src/blocks/testimonialcarousel/Save.js
+++ assets/reactjs/src/blocks/testimonialcarousel/Save.js
@@ -1,38 +1,37 @@
-const { Component,Fragment } = wp.element
+const { Component, Fragment } = wp.element
 const { RichText } = wp.editor
 import { animationAttr } from '../../components/HelperFunction'
 class Save extends Component {
+  renderAvatar = (avatar, avatarAlt) => {
+    return (
+      <Fragment>
+        {avatar.url != undefined ?
+          <img className="qubely-testimonial-avatar" src={avatar.url} alt={avatarAlt} />
+          :
+          <div className="qubely-image-placeholder qubely-testimonial-avatar"><i className="far fa-user"></i></div>
+        }
+      </Fragment>
+    )
+  }
   renderAuthorInfo = (item) => {
-    const { attributes: { showAvatar, avatarAlt, avatarLayout } } = this.props
+    const { attributes: { layout, showAvatar, avatarAlt, avatarLayout } } = this.props
     const { author, designation, avatar } = item
 
     return (
       <div className={`qubely-testimonial-author`}>
+
+        {(layout === 3 && showAvatar) && this.renderAvatar(avatar, avatarAlt)}
+
         <div className={showAvatar ? `qubely-testimonial-avatar-layout-${avatarLayout}` : ``}>
-          {showAvatar && (avatarLayout == 'left' || avatarLayout == 'top') &&
-            <Fragment>
-              {avatar.url != undefined ?
-                <img className="qubely-testimonial-avatar" src={avatar.url} alt={avatarAlt} />
-                :
-                <div className="qubely-image-placeholder qubely-testimonial-avatar"><i className="far fa-user"></i></div>
-              }
-            </Fragment>
-          }
+
+          {(layout !== 3 && showAvatar && (avatarLayout == 'left' || avatarLayout == 'top')) && this.renderAvatar(avatar, avatarAlt)}
 
           <div className="qubely-testimonial-author-info">
             <div className="qubely-testimonial-author-name"><RichText.Content value={author} /></div>
             <div className="qubely-testimonial-author-designation"><RichText.Content value={designation} /></div>
           </div>
 
-          {showAvatar && (avatarLayout == 'right' || avatarLayout == 'bottom') &&
-            <Fragment>
-              {avatar.url != undefined ?
-                <img className="qubely-testimonial-avatar" src={avatar.url} alt={avatarAlt} />
-                :
-                <div className="qubely-image-placeholder qubely-testimonial-avatar"><i className="far fa-user"></i></div>
-              }
-            </Fragment>
-          }
+          {(layout !== 3 && showAvatar && (avatarLayout == 'right' || avatarLayout == 'bottom')) && this.renderAvatar(avatar, avatarAlt)}
         </div>
       </div>
     )
@@ -44,10 +43,10 @@ class Save extends Component {
       const { message, ratings } = item
       return (
         <div key={index} className="js-item" >
-          <div className={`qubely-block-testimonial`}>
+          <div className={`qubely-block-testimonial-carousel qubely-layout-${layout}`}>
 
             {layout == 2 && this.renderAuthorInfo(item)}
-            {ratings > 0 && layout == 2 && <div className="qubely-testimonial-ratings" data-qubelyrating={ratings}></div>}
+            {(ratings > 0 && layout !== 1)&& <div className="qubely-testimonial-ratings" data-qubelyrating={ratings}></div>}
             {
               (quoteIcon && layout == 1) &&
               <div className="qubely-testimonial-quote">
@@ -55,8 +54,9 @@ class Save extends Component {
               </div>
             }
             <div className="qubely-testimonial-content"> <RichText.Content value={message} /></div>
-            {ratings > 0 && layout == 1 && <div className="qubely-testimonial-ratings" data-qubelyrating={ratings} />}
-            {layout == 1 && this.renderAuthorInfo(item)}
+            {(ratings > 0 && layout == 1) && <div className="qubely-testimonial-ratings" data-qubelyrating={ratings} />}
+            {layout === 3 && <span class="dashicons dashicons-arrow-down"></span>}
+            {layout !== 2 && this.renderAuthorInfo(item)}
 
             {
               (quoteIcon && layout == 2) &&
@@ -72,7 +72,7 @@ class Save extends Component {
   }
 
   render() {
-    const { attributes: { uniqueId, items, autoPlay, dragable, nav, dots, dotIndicator, interval, speed, animation } } = this.props
+    const { attributes: { uniqueId, layout, items, autoPlay, dragable, nav, dots, dotIndicator, interval, speed, animation } } = this.props
     let options = JSON.stringify({
       autoplay: autoPlay,
       items: items,
@@ -101,8 +101,10 @@ class Save extends Component {
     )
     return (
       <div className={`qubely-block-${uniqueId}`} {...animationAttr(animation)}>
-        <div className="js-slider" id="jsSlider1" data-options={options} >
-          {this.renderTestimonial()}
+        <div className={`qubely-block-testimonial-carousel qubely-layout-${layout}`}>
+          <div className="js-slider" id="jsSlider1" data-options={options} >
+            {this.renderTestimonial()}
+          </div>
         </div>
       </div>
     )
