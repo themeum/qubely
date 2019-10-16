c4023959ef8383600d752a19266d1c09d5e8f52e
faisal29 <faisalcse29@gmail.com>
2019-10-16 11:24:04 +0600

    fixed getCurrentPostId function in ParseCss.js file

--- assets/reactjs/src/helpers/ParseCss.js
+++ assets/reactjs/src/helpers/ParseCss.js
@@ -29,39 +29,39 @@ function innerBlocks(blocks, type = false) {
         if (blockName[0] === 'qubely' && attributes.uniqueId) {
             __CSS += CssGenerator(attributes, blockName[1], attributes.uniqueId, true)
 
-            if( typeof attributes['interaction'] !== 'undefined' ){
+            if (typeof attributes['interaction'] !== 'undefined') {
                 const { while_scroll_into_view, mouse_movement } = attributes.interaction
-                
-                if( typeof while_scroll_into_view !== 'undefined' && while_scroll_into_view.enable === true ){
-                    let {action_list} = while_scroll_into_view 
-                    action_list = action_list.sort( (a, b) => a.keyframe - b.keyframe )
-                    const interactionObj = { 
-                        blockId: attributes.uniqueId, 
-                        enable_mobile: typeof while_scroll_into_view.enable_mobile === 'undefined' ? false : while_scroll_into_view.enable_mobile, 
+
+                if (typeof while_scroll_into_view !== 'undefined' && while_scroll_into_view.enable === true) {
+                    let { action_list } = while_scroll_into_view
+                    action_list = action_list.sort((a, b) => a.keyframe - b.keyframe)
+                    const interactionObj = {
+                        blockId: attributes.uniqueId,
+                        enable_mobile: typeof while_scroll_into_view.enable_mobile === 'undefined' ? false : while_scroll_into_view.enable_mobile,
                         enable_tablet: typeof while_scroll_into_view.enable_tablet === 'undefined' ? false : while_scroll_into_view.enable_tablet,
                         animation: action_list
                     }
-                    let origin = { 
-                        x_offset: typeof while_scroll_into_view.transform_origin_x === 'undefined' ? 'center' : while_scroll_into_view.transform_origin_x, 
-                        y_offset: typeof while_scroll_into_view.transform_origin_y === 'undefined' ? 'center' : while_scroll_into_view.transform_origin_y, 
+                    let origin = {
+                        x_offset: typeof while_scroll_into_view.transform_origin_x === 'undefined' ? 'center' : while_scroll_into_view.transform_origin_x,
+                        y_offset: typeof while_scroll_into_view.transform_origin_y === 'undefined' ? 'center' : while_scroll_into_view.transform_origin_y,
                     }
                     interactionObj.origin = origin
-                    if( typeof interaction.while_scroll_view === 'undefined' ){
+                    if (typeof interaction.while_scroll_view === 'undefined') {
                         interaction.while_scroll_view = [interactionObj]
-                    }else{
+                    } else {
                         interaction.while_scroll_view.push(interactionObj)
                     }
                 }
-                if( typeof mouse_movement !== 'undefined' && mouse_movement.enable === true ){
+                if (typeof mouse_movement !== 'undefined' && mouse_movement.enable === true) {
                     const interactionObj = {
-                            blockId: attributes.uniqueId, 
-                            enable_mobile: typeof while_scroll_into_view.enable_mobile === 'undefined' ? false : while_scroll_into_view.enable_mobile, 
-                            enable_tablet: typeof while_scroll_into_view.enable_tablet === 'undefined' ? false : while_scroll_into_view.enable_tablet,
-                            animation: mouse_movement,
-                        }
-                    if( typeof interaction.mouse_movement === 'undefined' ){
+                        blockId: attributes.uniqueId,
+                        enable_mobile: typeof while_scroll_into_view.enable_mobile === 'undefined' ? false : while_scroll_into_view.enable_mobile,
+                        enable_tablet: typeof while_scroll_into_view.enable_tablet === 'undefined' ? false : while_scroll_into_view.enable_tablet,
+                        animation: mouse_movement,
+                    }
+                    if (typeof interaction.mouse_movement === 'undefined') {
                         interaction.mouse_movement = [interactionObj]
-                    }else{
+                    } else {
                         interaction.mouse_movement.push(interactionObj)
                     }
                 }
@@ -71,23 +71,24 @@ function innerBlocks(blocks, type = false) {
             innerBlocks(row.innerBlocks)
         }
     })
-    return {css: __CSS,interaction}
+    return { css: __CSS, interaction }
 }
 
 const ParseCss = (setDatabase = true) => {
     window.bindCss = true
-    const { getBlocks, getCurrentPostId } = select('core/block-editor')
-    let __blocks = { css: '', interaction:{} };
+    const { getBlocks } = select('core/block-editor')
+    const { getCurrentPostId } = select('core/editor')
+    let __blocks = { css: '', interaction: {} };
     if (typeof window.globalData != 'undefined') {
         __blocks.css += CssGenerator(window.globalData.settings, 'pagesettings', '8282882', true)
     }
-    let parseData = innerBlocks( getBlocks(), true )
+    let parseData = innerBlocks(getBlocks(), true)
     __blocks.interaction = parseData.interaction
     __blocks.css += parseData.css
-    if( __blocks.css !== '' ){
+    if (__blocks.css !== '') {
         localStorage.setItem('qubelyCSS', __blocks)
-        if(setDatabase){
-            API_fetch(getCurrentPostId(), __blocks).then( data => {} )
+        if (setDatabase) {
+            API_fetch(getCurrentPostId(), __blocks).then(data => { })
         }
     }
     setTimeout(() => {
