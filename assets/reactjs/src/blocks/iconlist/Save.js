a1b89ad585ba4eeee14e71776ed178f72812fb42
faisal29 <faisalcse29@gmail.com>
2019-11-19 17:42:01 +0600

    added animation in iconlist block

--- assets/reactjs/src/blocks/iconlist/Save.js
+++ assets/reactjs/src/blocks/iconlist/Save.js
@@ -1,6 +1,6 @@
 const { Component } = wp.element;
 const { RichText } = wp.blockEditor
-const { HelperFunction: { IsInteraction } } = wp.qubelyComponents
+const { HelperFunction: { IsInteraction, animationAttr } } = wp.qubelyComponents
 
 class Save extends Component {
 
@@ -9,20 +9,20 @@ class Save extends Component {
         return listItems.map((item, index) => {
             return (
                 <li className={`qubely-list-li`}>
-                    { iconPosition == 'left' && <span className={`qubely-list-item-icon ${item.icon} fa-fw`} /> }
+                    {iconPosition == 'left' && <span className={`qubely-list-item-icon ${item.icon} fa-fw`} />}
                     <RichText.Content tagName="span" value={item.text} />
-                    { iconPosition == 'right' && <span className={`qubely-list-item-icon ${item.icon} fa-fw`} /> }
+                    {iconPosition == 'right' && <span className={`qubely-list-item-icon ${item.icon} fa-fw`} />}
                 </li>
             )
         })
 
     }
-    
+
     render() {
-        const { attributes: { uniqueId, interaction } } = this.props
+        const { attributes: { uniqueId, interaction, animation } } = this.props
         const interactionClass = IsInteraction(interaction) ? 'qubley-block-interaction' : '';
         return (
-            <div className={`qubely-block-${uniqueId}`}>
+            <div className={`qubely-block-${uniqueId}`} {...animationAttr(animation)}>
                 <div className={`qubely-block-icon-list ${interactionClass}`}>
                     <ul className="qubely-list">
                         {this.renderListItems()}
