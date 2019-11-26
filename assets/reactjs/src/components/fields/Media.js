0c4a8ad61f7d46994eab36eb7ceacf057704ed6d
faisal29 <faisalcse29@gmail.com>
2019-11-26 13:14:42 +0600

    updated media component with video preview option in panel

--- assets/reactjs/src/components/fields/Media.js
+++ assets/reactjs/src/components/fields/Media.js
@@ -1,4 +1,4 @@
-const {__} = wp.i18n
+const { __ } = wp.i18n
 import '../css/media.scss'
 const { Component } = wp.element;
 const { MediaUpload } = wp.blockEditor;
@@ -6,101 +6,101 @@ export const { Tooltip, Dashicon } = wp.components;
 
 class Media extends Component {
 
-	setSettings( media ) {
-		const { multiple, onChange,value} = this.props
-		if( multiple ){
+	setSettings(media) {
+		const { multiple, onChange, value } = this.props
+		if (multiple) {
 			let medias = [];
-			media.forEach( single => {
-				if ( single && single.url ){
-					medias.push( {url: single.url, id: single.id} );
+			media.forEach(single => {
+				if (single && single.url) {
+					medias.push({ url: single.url, id: single.id });
 				}
 			});
-			onChange( value ? value.concat(medias) : medias );
-		}else{
-			if ( media && media.url ) {
-				onChange({url: media.url, id: media.id});
+			onChange(value ? value.concat(medias) : medias);
+		} else {
+			if (media && media.url) {
+				onChange({ url: media.url, id: media.id });
 			}
 		}
 	}
 
-	removeImage( id ){
+	removeImage(id) {
 		const { multiple, onChange } = this.props
-		if( multiple ){
+		if (multiple) {
 			let value = (this.props.value).slice()
 			value.splice(id, 1)
-			onChange( value )
-		}else{
+			onChange(value)
+		} else {
 			onChange({})
 		}
 	}
 
-	isUrl(url){
-		if( ['wbm','jpg','jpeg','gif','png', 'svg'].indexOf( url.split('.').pop().toLowerCase() ) != -1 ){
+	isUrl(url) {
+		if (['wbm', 'jpg', 'jpeg', 'gif', 'png', 'svg'].indexOf(url.split('.').pop().toLowerCase()) != -1) {
 			return url;
-		}else{
-			return qubely_admin.plugin+'assets/img/qubely-medium.jpg';
+		} else {
+			return qubely_admin.plugin + 'assets/img/qubely-medium.jpg';
 		}
 	}
-	
+
 
 	render() {
-		const { type,multiple,value,panel } = this.props;
+		const { type, multiple, value, panel, video } = this.props;
 		return (
 			<div className='qubely-media'>
-				{ this.props.label &&
+				{this.props.label &&
 					<label>{this.props.label}</label>
 				}
 				<MediaUpload
-					onSelect={ val => this.setSettings(val) }
-					allowedTypes={ type || ['image'] }
-					multiple={ multiple || false }
-					value={ value }
+					onSelect={val => this.setSettings(val)}
+					allowedTypes={type.length ? [...type] : ['image']}
+					multiple={multiple || false}
+					value={value}
 					render={({ open }) => (
 						<div className="qubely-single-img">
 							<div>
-								{ multiple ?
+								{multiple ?
 									<div>
-										{ ( value.length > 0 ) &&
-											value.map( (v,index) => {
-												return(
+										{(value.length > 0) &&
+											value.map((v, index) => {
+												return (
 													<span className="qubely-media-image-parent">
-														<img src={this.isUrl(v.url)} alt={__('image')}/>
-														{ panel &&
+														<img src={this.isUrl(v.url)} alt={__('image')} />
+														{panel &&
 															<div className="qubely-media-actions qubely-field-button-list">
 																<Tooltip text={__('Edit')}>
 																	<button className="qubely-button" aria-label={__('Edit')} onClick={open} role="button">
-																		<span aria-label={__('Edit')} className="fas fa-pencil-alt fa-fw"/>
+																		<span aria-label={__('Edit')} className="fas fa-pencil-alt fa-fw" />
 																	</button>
 																</Tooltip>
 																<Tooltip text={__('Remove')}>
-																	<button className="qubely-button" aria-label={__('Remove')} onClick={ () => this.removeImage(index) } role="button">
-																		<span aria-label={__('Close')} className="far fa-trash-alt fa-fw"/>
+																	<button className="qubely-button" aria-label={__('Remove')} onClick={() => this.removeImage(index)} role="button">
+																		<span aria-label={__('Close')} className="far fa-trash-alt fa-fw" />
 																	</button>
-																</Tooltip>		
+																</Tooltip>
 															</div>
 														}
 													</span>
 												)
-											} )
+											})
 										}
 										<div onClick={open} className={"qubely-placeholder-image"}><Dashicon icon="insert" /><span>{__('Insert')}</span></div>
 									</div>
 									:
-									( ( value && value.url ) ? 
-										<span className="qubely-media-image-parent"> 
-											<img src={this.isUrl(value.url)} alt={__('image')}/>
-											{ panel &&
+									((value && value.url) ?
+										<span className="qubely-media-image-parent">
+											<video controls autoPlay loop src={value.url} />
+											{panel &&
 												<div className="qubely-media-actions qubely-field-button-list">
 													<Tooltip text={__('Edit')}>
 														<button className="qubely-button" aria-label={__('Edit')} onClick={open} role="button">
-															<span aria-label={__('Edit')} className="fas fa-pencil-alt fa-fw"/>
+															<span aria-label={__('Edit')} className="fas fa-pencil-alt fa-fw" />
 														</button>
 													</Tooltip>
 													<Tooltip text={__('Remove')}>
-														<button className="qubely-button" aria-label={__('Remove')} onClick={ () => this.removeImage(value.id) } role="button">
-															<span aria-label={__('Close')} className="far fa-trash-alt fa-fw"/>
+														<button className="qubely-button" aria-label={__('Remove')} onClick={() => this.removeImage(value.id)} role="button">
+															<span aria-label={__('Close')} className="far fa-trash-alt fa-fw" />
 														</button>
-													</Tooltip>		
+													</Tooltip>
 												</div>
 											}
 										</span>
