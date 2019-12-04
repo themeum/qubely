e2135cc6fc5e78efd7cd05441f686eb4e01068c8
Delowar Hosain <delowar6494@gmail.com>
2019-12-04 13:43:41 +0600

    Pie Progress Settings added

--- assets/reactjs/src/blocks/pieprogress/Progress.js
+++ assets/reactjs/src/blocks/pieprogress/Progress.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const defaultProps = {
     size: 150,
     thickness: 8,
--- assets/reactjs/src/blocks/pieprogress/Save.js
+++ assets/reactjs/src/blocks/pieprogress/Save.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import Progress from './Progress'
 const { __ } = wp.i18n
 
