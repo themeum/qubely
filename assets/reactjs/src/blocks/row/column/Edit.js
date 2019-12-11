f468218870bf47f2e6e53c0d512fd4db1d64205f
faisal29 <faisalcse29@gmail.com>
2019-12-11 14:22:57 +0600

    changed default placeholder for column in row

--- assets/reactjs/src/blocks/row/column/Edit.js
+++ assets/reactjs/src/blocks/row/column/Edit.js
@@ -304,7 +304,7 @@ class Edit extends Component {
             isSelected,
             clientId
         } = this.props
-        
+
         const { rowWidth, resizing, responsiveDevice } = this.state
         const { columns, nextBlockId, blockIndex } = this.checkColumnStatus()
 
@@ -315,6 +315,8 @@ class Edit extends Component {
         if (resizing) {
             resigingClass += ' is-resizing'
         }
+        const { getBlockOrder } = select('core/block-editor')
+        let hasChildBlocks = getBlockOrder(clientId).length > 0
         if (uniqueId) { CssGenerator(this.props.attributes, 'column', uniqueId); }
 
         return (
@@ -408,7 +410,14 @@ class Edit extends Component {
 
                         <div className={`qubely-column qubely-column-admin qubely-block-${uniqueId}`} data-column-width={this.props.attributes.colWidth.md}>
                             <div className={`qubely-column-inner`}>
-                                <InnerBlocks templateLock={false} />
+                                <InnerBlocks
+                                    templateLock={false}
+                                    renderAppender={(
+                                        hasChildBlocks ?
+                                            undefined :
+                                            () => <InnerBlocks.ButtonBlockAppender />
+                                    )}
+                                />
                             </div>
                         </div>
                     </ResizableBox>
