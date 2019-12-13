f01672f559397b9230f04618b490e68db7124193
Delowar <delowardev@gmail.com>
2019-12-13 13:17:44 +0600

    Pie: Save File Updated

--- assets/reactjs/src/blocks/pieprogress/Save.js
+++ assets/reactjs/src/blocks/pieprogress/Save.js
@@ -9,6 +9,15 @@ const Save = (props) => {
         size,
         layout,
         corner,
+        enableIcon,
+        iconStyle,
+        iconText,
+        iconName,
+        image,
+        imageAlt,
+        enableHeading,
+        headingPosition,
+        heading
     } = props.attributes
 
     const progressAttr = {
@@ -18,11 +27,50 @@ const Save = (props) => {
         uniqueId,
         percent: progress
     }
+
     return (
         <div className={`qubely-block-${uniqueId} qubely-block-pie-progress`}>
             <div className="qubely-progress-parent">
                 <Progress {...progressAttr} />
+                {enableIcon && (
+                    <div className="qubely-progress-inner-text">
+                        {iconStyle === 'text' && (
+                            <RichText.Content
+                                value={iconText}
+                            />
+                        )}
+                        {iconStyle === 'icon' && (
+                            <span className={`qubely-pie-icon ${iconName}`} />
+                        )}
+                        {iconStyle === 'image' && (
+                            <div className={'icon-image ' + (image.url === undefined && 'pie-placeholder')}>
+                                {
+                                    image.url !== undefined ? (
+                                        <img className="qubely-pie-image" src={image.url} alt={imageAlt && imageAlt} />
+                                    ) : (
+                                            <span className="qubely-pie-placeholder far fa-image" />
+                                        )
+                                }
+                            </div>
+                        )}
+
+                        {(enableHeading && headingPosition === 'inside') && (
+                            <RichText.Content
+                                value={heading}
+                                className="qubely-pie-progress-heading"
+                            />
+                        )}
+                    </div>
+                )}
             </div>
+
+
+            {(enableHeading && headingPosition === 'outside') && (
+                <RichText.Content
+                    value={heading}
+                    className="qubely-pie-progress-heading qubely-outside"
+                />
+            )}
         </div>
     )
 }
