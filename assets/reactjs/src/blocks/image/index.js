7bf1417f43ab195afcdae2da98b4005ce9c20a95
faisalahmedbd <faisalcse29@gmail.com>
2020-03-02 17:31:33 +0600

    Url option in Image block - blurb layout

--- assets/reactjs/src/blocks/image/index.js
+++ assets/reactjs/src/blocks/image/index.js
@@ -640,6 +640,108 @@ registerBlockType('qubely/image', {
                     </div>
                 );
             }
+        },
+        {
+            attributes,
+            save(props) {
+                const {
+                    attributes: {
+                        uniqueId,
+                        layout,
+                        imageUrl,
+                        image,
+                        imageType,
+                        image2x,
+                        imgAlt,
+                        externalImageUrl,
+                        animateOnHover,
+                        titleVisibleOnHover,
+                        subTitleVisibleOnHover,
+                        title,
+                        titleLevel,
+                        subTitle,
+                        enableSubTitle,
+                        imageCaption,
+                        enableCaption,
+                        contentAnimation,
+                        contentVerticalAlign,
+                        contentAlignment,
+                        enableFrame,
+                        frameAnimateOnHover,
+                        interaction,
+                        animation
+                    }
+                } = props
+
+                const titleTagName = 'h' + titleLevel;
+                const interactionClass = IsInteraction(interaction) ? 'qubley-block-interaction' : '';
+                const renderImage = () => {
+                    return (
+                        <Fragment>
+                            {
+                                (imageType === 'local' && image.url != undefined) ?
+                                    <Fragment>
+                                        {image2x.url != undefined ?
+                                            <img className="qubely-image-image" src={image.url} srcset={image.url + ' 1x, ' + image2x.url + ' 2x'} alt={imgAlt && imgAlt} />
+                                            :
+                                            <img className="qubely-image-image" src={image.url} alt={imgAlt && imgAlt} />
+                                        }
+                                    </Fragment>
+                                    :
+                                    (imageType === 'external' && externalImageUrl.url != undefined) ?
+                                        <img className="qubely-image-image" src={externalImageUrl.url} alt={imgAlt && imgAlt} />
+                                        :
+                                        <div className="qubely-image-image qubely-image-placeholder"><i className="far fa-image" /></div>
+                            }
+                        </Fragment>
+                    )
+                }
+                return (
+
+                    <div className={`qubely-block-${uniqueId}`}  {...animationAttr(animation)}>
+                        <div className={`qubely-block-image ${interactionClass} qubely-image-layout-${layout}`}>
+                            <div className={`qubely-image-media${(layout == 'blurb' && animateOnHover == 1) ? ' qubely-hover-animation-on' : ''}${(layout == 'blurb' && animateOnHover == 1) ? ' qubely-hover-animation-type-' + contentAnimation : ''} qubely-vertical-alignment-${contentVerticalAlign} qubely-horizontal-alignment-${contentAlignment}${enableFrame == 1 ? ((animateOnHover == 1 && frameAnimateOnHover == 1) ? ' qubely-has-frame qubely-frame-animate-on-hover' : ' qubely-has-frame') : ''}`}>
+                                <figure>
+                                    <div className="qubely-image-container">
+                                        {
+                                            (imageUrl.url && layout === 'simple') ?
+                                                <a href={imageUrl.url ? `//${imageUrl.url}` : '#'} {...(imageUrl.target && { target: '_blank' })} {...(imageUrl.nofollow ? { rel: 'nofollow noopener noreferrer' } : { ...imageUrl.target && { rel: 'noopener noreferrer' } })}>
+                                                    {renderImage()}
+                                                </a>
+                                                : renderImage()
+                                        }
+
+                                        {layout == 'blurb' &&
+                                            <div className="qubely-image-content">
+                                                <div className="qubely-image-content-inner">
+                                                    <RichText.Content
+                                                        tagName={titleTagName}
+                                                        className={`qubely-image-title${(animateOnHover == 1 && enableSubTitle == 1 && subTitleVisibleOnHover == 1 && titleVisibleOnHover != 1) ? ' qubely-visible-on-hover-enabled' : ''}${(animateOnHover == 1 && titleVisibleOnHover == 1) ? ' qubely-visible-on-hover' : ''}`}
+                                                        value={title} />
+
+                                                    {enableSubTitle == 1 &&
+                                                        <RichText.Content
+                                                            tagName='div'
+                                                            className={`qubely-image-sub-title${(animateOnHover == 1 && subTitleVisibleOnHover == 1 && titleVisibleOnHover != 1) ? ' qubely-visible-on-hover-enabled' : ''}${titleVisibleOnHover == 1 ? ' qubely-visible-on-hover' : (animateOnHover == 1 && subTitleVisibleOnHover == 1) ? ' qubely-visible-on-hover' : ''}`}
+                                                            value={subTitle} />
+                                                    }
+                                                </div>
+                                            </div>
+                                        }
+                                    </div>
+
+                                    {(layout == 'simple' && enableCaption == 1) &&
+                                        <RichText.Content
+                                            tagName='figcaption'
+                                            className="qubely-image-caption"
+                                            value={imageCaption} />
+                                    }
+                                </figure>
+                            </div>
+                        </div>
+                    </div>
+                );
+            }
         }
     ]
 });
