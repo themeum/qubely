3bf791c59a41419c2bc92c0b596d4d6c31609e45
faisalahmedbd <faisalcse29@gmail.com>
2020-03-18 15:03:31 +0600

    tab

--- assets/reactjs/src/blocks/tabs/Edit.js
+++ assets/reactjs/src/blocks/tabs/Edit.js
@@ -1,12 +1,56 @@
-const { __ } = wp.i18n
-const { Tooltip, PanelBody, Toolbar } = wp.components;
-const { compose } = wp.compose
-const { withSelect, withDispatch } = wp.data
-const { Component, Fragment } = wp.element;
-const { InnerBlocks, RichText, InspectorControls, BlockControls } = wp.blockEditor
-const { Color, IconList, Select, Styles, Typography, Range, RadioAdvanced, gloalSettings: { globalSettingsPanel, animationSettings, interactionSettings }, Inline: { InlineToolbar }, BoxShadow, Alignment, Tabs, Tab, Separator, Border, Padding, BorderRadius, CssGenerator: { CssGenerator }, withCSSGenerator, InspectorTabs,
-	InspectorTab } = wp.qubelyComponents
 import icons from '../../helpers/icons';
+const { __ } = wp.i18n;
+const {
+	Tooltip,
+	PanelBody,
+	Toolbar
+} = wp.components;
+
+const { compose } = wp.compose;
+
+const {
+	withSelect,
+	withDispatch } = wp.data;
+
+const {
+	Component,
+	Fragment
+} = wp.element;
+
+const {
+	InnerBlocks,
+	RichText,
+	InspectorControls,
+	BlockControls
+} = wp.blockEditor;
+
+const {
+	Color,
+	IconList,
+	Select,
+	Styles,
+	Typography,
+	Range,
+	RadioAdvanced,
+	gloalSettings: {
+		globalSettingsPanel,
+		animationSettings,
+		interactionSettings
+	},
+	Inline: {
+		InlineToolbar
+	},
+	BoxShadow,
+	Alignment,
+	Tabs,
+	Tab,
+	Separator,
+	Border,
+	Padding,
+	BorderRadius,
+	withCSSGenerator,
+	InspectorTabs,
+	InspectorTab } = wp.qubelyComponents
 
 class Edit extends Component {
 
