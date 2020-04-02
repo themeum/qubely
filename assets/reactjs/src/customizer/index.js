c502d52ff471feef31d5ddd0e1b361b3dc7dd0a0
faisalahmedbd <faisalcse29@gmail.com>
2020-04-02 16:12:15 +0600

    init global settings

--- assets/reactjs/src/customizer/index.js
+++ assets/reactjs/src/customizer/index.js
@@ -1,6 +1,7 @@
-const { registerPlugin } = wp.plugins;
-import Plugin from './plugin' 
+
+import Plugin from './plugin';
 import icons from '../helpers/icons';
+const { registerPlugin } = wp.plugins;
 
 registerPlugin( 'qubely-customizer', {
     icon: icons.qubely,
