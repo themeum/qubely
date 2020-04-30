4d4c8fee6907706f7e47da4b4bcbf627f35bcbf0
delowardev <delowardev@gmail.com>
2020-04-30 19:32:19 +0600

    New settings page added

--- core/admin-views/settings.php
+++ core/admin-views/settings.php
@@ -6,28 +6,33 @@
         <a href="#advanced" class="nav-tab">Advanced</a>
     </div>
 
-    <div id="qubely-settings-tabs-content">
+    <form id="qubely-settings-tabs-content">
+
+        <?php
+            $option_data    = get_option('qubely_options');
+        ?>
+
         <div class="qubely-settings-inner" id="general">
             <table class="form-table">
                 <tbody>
                     <tr>
                         <th class="row"> Google map API key </th>
                         <td>
-                            <input type="text" class="regular-text">
+                            <input name="qubely_options[gmap_api]" type="text" class="regular-text">
                             <p class="description">Enter your google map api key</p>
                         </td>
                     </tr>
                     <tr>
                         <th class="row"> ReCaptcha site key </th>
                         <td>
-                            <input type="text" class="regular-text">
+                            <input name="qubely_options[recaptcha_site_key]" type="text" class="regular-text">
                             <p class="description">Enter your reCaptcha secret key</p>
                         </td>
                     </tr>
                     <tr>
                         <th class="row"> ReCaptch secret key </th>
                         <td>
-                            <input type="text" class="regular-text">
+                            <input name="qubely_options[recaptcha_secret_key]" type="text" class="regular-text">
                             <p class="description">Enter your reCaptcha secret key</p>
                         </td>
                     </tr>
@@ -38,8 +43,30 @@
             Style Settings
         </div>
         <div class="qubely-settings-inner" id="advanced" style="display: none">
-            Advanced Settings
+            <table class="form-table">
+                <tr>
+                    <th scope="row"><?php esc_html_e('CSS Save Method', 'qubely'); ?></th>
+                    <td>
+                        <?php $value = isset($option_data['css_save_as']) ? $option_data['css_save_as'] : 'wp_head' ; ?>
+                        <select name="qubely_options[css_save_as]">
+                            <?php
+                            $options = array(
+                                'wp_head'   => __('Header', 'qubely'),
+                                'filesystem' => __('File System', 'qubely'),
+                            );
+                            foreach ($options as $id => $label) { ?>
+                                <option value="<?php echo esc_attr($id); ?>" <?php selected($value, $id, true); ?>>
+                                    <?php echo strip_tags($label); ?>
+                                </option>
+                            <?php }
+                            ?>
+                        </select>
+                        <p class="description"> <?php _e('Select where you want to save CSS.', 'qubely'); ?></p>
+                    </td>
+                </tr>
+            </table>
         </div>
-    </div>
+        <?php submit_button('Save Changes');?>
+    </form>
 
 </div>
