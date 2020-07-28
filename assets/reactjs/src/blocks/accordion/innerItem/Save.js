a9be0045573a92ca73634fdc4b6bd4e659fafe33
faisalahmedbd <faisalcse29@gmail.com>
2020-07-28 16:21:56 +0600

    Backward compatibility for Richsnippet in accordion block

--- assets/reactjs/src/blocks/accordion/innerItem/Save.js
+++ assets/reactjs/src/blocks/accordion/innerItem/Save.js
@@ -3,13 +3,13 @@ const { InnerBlocks, RichText } = wp.blockEditor;
 const { HelperFunction: { animationAttr } } = wp.qubelyComponents
 class Save extends Component {
     render() {
-        const { uniqueId, itemNumber, heading, panelIcon, iconPosition, fillType, animation, openFirstItem, richSnippet } = this.props.attributes;
+        const { uniqueId, itemNumber, heading, panelIcon, iconPosition, fillType, animation, openFirstItem, richSnippet = undefined } = this.props.attributes;
         const className = `qubely-accordion-item qubely-type-${fillType} ${openFirstItem && itemNumber == 0 && 'qubely-accordion-active'}`;
-        
+
         const mainEntryProp = richSnippet ? {
             itemscope: true,
-            itemprop:"mainEntity",
-            itemtype:"https://schema.org/Question"
+            itemprop: "mainEntity",
+            itemtype: "https://schema.org/Question"
         } : {}
 
         const itemPropName = richSnippet ? {
@@ -18,28 +18,36 @@ class Save extends Component {
 
         const itemPropAnswer = richSnippet ? {
             itemscope: true,
-            itemprop:"acceptedAnswer",
-            itemtype:"https://schema.org/Answer" 
+            itemprop: "acceptedAnswer",
+            itemtype: "https://schema.org/Answer"
         } : {}
 
         return (
             <div className={`qubely-block-${uniqueId}`} {...animationAttr(animation)}>
-                <div {...mainEntryProp} className={className}>
+                <div
+                    {...(typeof richSnippet !== 'undefined' && mainEntryProp)}
+                    className={className}>
                     <div className={`qubely-accordion-panel ${panelIcon && 'qubely-icon-position-' + iconPosition}`}>
                         <span className="qubely-accordion-panel-handler" role="button">
                             {(panelIcon && iconPosition == 'left') && <span className={`qubely-accordion-icon ${panelIcon}`} />}
-                            <RichText.Content {...itemPropName} tagName="span" className="qubely-accordion-panel-handler-label" value={heading} />
+                            <RichText.Content {...(typeof richSnippet !== 'undefined' && itemPropName)} tagName="span" className="qubely-accordion-panel-handler-label" value={heading} />
                             {(panelIcon && iconPosition == 'right') && <span className={`qubely-accordion-icon ${panelIcon}`} />}
                         </span>
                     </div>
-                    <div 
-                        {...itemPropAnswer}
-                        className="qubely-accordion-body" 
+                    <div
+                        {...(typeof richSnippet !== 'undefined' && itemPropAnswer)}
+                        className="qubely-accordion-body"
                         style={openFirstItem && itemNumber == 0 && 'display: block;'}
                     >
-                        <div itemprop="text">
-                            <InnerBlocks.Content/>
-                        </div>
+                        {
+                            typeof richSnippet !== 'undefined' ?
+                                <div itemprop="text">
+                                    <InnerBlocks.Content />
+                                </div>
+                                :
+                                <InnerBlocks.Content />
+                        }
+
                     </div>
                 </div>
             </div>
