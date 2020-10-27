5b2e5fb82946f8c357b5347c8b28846d9fed7653
Zaman <itszamanq@gmail.com>
2020-10-27 18:16:55 +0600

    cpt-support-init

--- assets/reactjs/src/index.js
+++ assets/reactjs/src/index.js
@@ -109,3 +109,37 @@ function appendImportButton() {
 function qubelyImportLayout() {
     ModalManager.open(<PageListModal rowClientId={false} />);
 }
+
+/**
+ * Determines if _ is lodash or not
+ */
+function isLodash() {
+    let isLodash = false;
+
+    // If _ is defined and the function _.forEach exists then we know underscore OR lodash are in place
+    if ('undefined' != typeof (_) && 'function' == typeof (_.forEach)) {
+
+        // A small sample of some of the functions that exist in lodash but not underscore
+        const funcs = ['get', 'set', 'at', 'cloneDeep'];
+
+        // Simplest if assume exists to start
+        isLodash = true;
+
+        funcs.forEach(function (func) {
+            // If just one of the functions do not exist, then not lodash
+            isLodash = ('function' != typeof (_[func])) ? false : isLodash;
+        });
+    }
+
+    if (isLodash) {
+        // We know that lodash is loaded in the _ variable
+        return true;
+    } else {
+        // We know that lodash is NOT loaded
+        return false;
+    }
+};
+
+if (isLodash()) {
+    _.noConflict();
+}
