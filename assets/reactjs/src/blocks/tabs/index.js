a0d65a0b48d89a7fd7db4e8b2f336258b27568a8
faisalahmedbd <faisalcse29@gmail.com>
2020-11-05 12:48:39 +0600

    Tab block - added depricated support

--- assets/reactjs/src/blocks/tabs/index.js
+++ assets/reactjs/src/blocks/tabs/index.js
@@ -2,8 +2,17 @@ import './style.scss';
 import Save from './Save';
 import Edit from './Edit';
 import attributes from './attributes';
+const { Component } = wp.element;
+const { InnerBlocks } = wp.blockEditor;
+const {
+    HelperFunction: {
+        animationAttr,
+        IsInteraction
+    }
+} = wp.qubelyComponents;
+
 const { __ } = wp.i18n;
-const { registerBlockType } = wp.blocks; 
+const { registerBlockType } = wp.blocks;
 
 registerBlockType('qubely/tabs', {
     title: __('Tabs'),
@@ -44,5 +53,49 @@ registerBlockType('qubely/tabs', {
     },
     attributes,
     edit: Edit,
-    save: Save
+    save: Save,
+    deprecated: [
+        {
+            attributes,
+            save(props) {
+                const {
+                    attributes: {
+                        uniqueId,
+                        tabStyle,
+                        tabTitles,
+                        iconPosition,
+                        navAlignment,
+                        animation,
+                        interaction
+                    }
+                } = props;
+
+                const interactionClass = IsInteraction(interaction) ? 'qubley-block-interaction' : '';
+
+                const renderTabTitles = () => {
+                    return tabTitles.map((title, index) =>
+                        <span className={`qubely-tab-item ${(index == 0) ? 'qubely-active' : ''}`}>
+                            <span class={`qubely-tab-title ${title.iconName ? 'qubely-has-icon-' + iconPosition : ''}`} role="button">
+                                {title.iconName && (iconPosition == 'top' || iconPosition == 'left') && (<i className={`qubely-tab-icon ${title.iconName}`} />)}
+                                {title.title}
+                                {title.iconName && (iconPosition == 'right') && (<i className={`qubely-tab-icon ${title.iconName}`} />)}
+                            </span>
+                        </span>
+                    );
+                }
+                return (
+                    <div className={`qubely-block-${uniqueId}`} {...animationAttr(animation)}>
+                        <div className={`qubely-block-tab ${interactionClass} qubely-tab-style-${tabStyle}`}>
+                            <div className={`qubely-tab-nav qubely-alignment-${navAlignment}`}>
+                                {renderTabTitles()}
+                            </div>
+                            <div className={`qubely-tab-body`}>
+                                <InnerBlocks.Content />
+                            </div>
+                        </div>
+                    </div>
+                );
+            }
+        }
+    ]
 })
