7c9763b3fc3869b79b4e723dfdb7ab0732721fd7
faisalahmedbd <faisalcse29@gmail.com>
2020-11-23 16:38:05 +0600

    Column - removed extra padding & margin and downgraded babel packages to previous versions

--- qubely.php
+++ qubely.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Qubely - Advanced Gutenberg Blocks
  * Plugin URI:        https://www.themeum.com/
  * Description:       The one and only Gutenberg block plugin you will ever need.
- * Version: 		  1.6.1
+ * Version: 		  1.6.2
  * Author:            Themeum.com
  * Author URI:        https://www.themeum.com/
  * Text Domain:       qubely
@@ -24,7 +24,7 @@ function qubely_language_load()
 }
 
 // Define Version
-define('QUBELY_VERSION', '1.6.1');
+define('QUBELY_VERSION', '1.6.2');
 
 // Define License
 define('QUBELY_LICENSE', 'free');
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://www.themeum.com
 Tags: blocks, gutenberg, gutenberg blocks, editor, block, page builder
 Requires at least: 5.0
 Tested up to: 5.5
-Stable tag: 1.6.1
+Stable tag: 1.6.2
 Requires PHP: 7.0
 License: GPL-2.0+
 License URI: http://www.gnu.org/licenses/gpl-2.0.txt
