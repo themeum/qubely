5a8ed3e3fb6c469ce98b628dafab00541d808b28
Jakir Hossen <jakir@M1Pro.local>
2022-01-13 12:04:23 +0600

    after remove some code and stable version

--- assets/reactjs/src/hooks/with-css-generator/index.js
+++ assets/reactjs/src/hooks/with-css-generator/index.js
@@ -25,12 +25,9 @@ export default function withCSSGenerator() {
         const blockAttributes = wp.blocks.getBlockType(
           this.props.name
         ).attributes;
-        let responsiveCSS = {},
-          nonResponsiveCSS = {};
 
         const saveinState = (value) => {
-          responsiveCSS = value.responsiveCSS;
-          nonResponsiveCSS = value.nonResponsiveCSS;
+          return;
           this.setState({
             responsiveCSS: value.responsiveCSS,
             nonResponsiveCSS: value.nonResponsiveCSS,
@@ -40,10 +37,10 @@ export default function withCSSGenerator() {
         generateCSS(blockAttributes, attributes, (value) => saveinState(value));
 
         if (uniqueId) {
-          this.saveCSS(responsiveCSS, nonResponsiveCSS);
+          this.saveCSS();
         }
       };
-      saveCSS = (responsiveCSS, nonResponsiveCSS) => {
+      saveCSS = () => {
         const {
           attributes,
           attributes: { uniqueId },
@@ -58,156 +55,11 @@ export default function withCSSGenerator() {
         CssGenerator(attributes, blockName[1], uniqueId, false);
       };
 
-      componentDidUpdate(prevProps, prevState) {
-        const {
-          name,
-          clientId,
-          attributes,
-          attributes: { uniqueId },
-        } = this.props;
-        const { responsiveCSS, nonResponsiveCSS } = this.state;
-        let isLayoutChanged = false,
-          blockAttributes = wp.blocks.getBlockType(this.props.name).attributes,
-          changedAttributes = Object.keys(
-            diff(prevProps.attributes, attributes)
-          );
-
-        const { getBlock, getBlocks, getBlockRootClientId, getBlockIndex } =
-          wp.data.select("core/block-editor");
-        const currentBlock = getBlock(clientId);
-
-        const rootBlockClientID = (clientId) => {
-          if (getBlockRootClientId(clientId)) {
-            return rootBlockIndex(getBlockRootClientId(clientId));
-          } else {
-            return clientId;
-          }
-        };
-        let isDuplicateBlock = false;
-
-        const isDuplicatingBlock = (allBlocks) => {
-          if (allBlocks.length === 0) {
-            return;
-          }
-          if (prevProps.attributes.uniqueId !== uniqueId) {
-            for (let index = 0; index < allBlocks.length; index++) {
-              if (
-                allBlocks[index].attributes.uniqueId ===
-                prevProps.attributes.uniqueId
-              ) {
-                isDuplicateBlock = true;
-                break;
-              } else if (allBlocks[index].innerBlocks.length > 0) {
-                isDuplicatingBlock(allBlocks[index].innerBlocks);
-                if (isDuplicateBlock) {
-                  break;
-                }
-              }
-            }
-          }
-          return isDuplicateBlock;
-        };
-
-        if (changedAttributes.length > 0) {
-          if (
-            changedAttributes.indexOf("layout") !== -1 ||
-            changedAttributes.indexOf("style") !== -1 ||
-            changedAttributes.indexOf("recreateStyles") !== -1 ||
-            changedAttributes.indexOf("fillType") !== -1 ||
-            changedAttributes.indexOf("iconStyle") !== -1 ||
-            changedAttributes.indexOf("buttonFillType") !== -1 ||
-            changedAttributes.indexOf("tabStyle") !== -1 ||
-            changedAttributes.indexOf("separatorStyle") !== -1
-          ) {
-            isLayoutChanged = true;
-            this.saveStyleAttributes();
-          }
-          if (!isLayoutChanged) {
-            if (changedAttributes.indexOf("uniqueId") !== -1) {
-              let currentStyleElement = window.document.getElementById(
-                "qubely-block-" + prevProps.attributes.uniqueId
-              );
-              if (currentStyleElement && !isDuplicatingBlock(getBlocks())) {
-                currentStyleElement.id = "qubely-block-" + attributes.uniqueId;
-                let newStyle = currentStyleElement.innerHTML.replace(
-                  new RegExp(`${prevProps.attributes.uniqueId}`, "g"),
-                  `${attributes.uniqueId}`
-                );
-                currentStyleElement.innerHTML = newStyle;
-              } else {
-                this.saveCSS(responsiveCSS, nonResponsiveCSS);
-              }
-            }
-
-            changedAttributes = changedAttributes.filter(
-              (attr) => attr !== "uniqueId"
-            );
-
-            let newState = {
-              nonResponsiveCSS: nonResponsiveCSS,
-              responsiveCSS: responsiveCSS,
-            };
-
-            if (changedAttributes.length > 0) {
-              const updateState = (attribute, key, value) => {
-                if (typeof key === "undefined") {
-                  newState = {
-                    ...newState,
-                    nonResponsiveCSS: {
-                      ...newState.nonResponsiveCSS,
-                      ...{ [attribute]: value },
-                    },
-                  };
-                } else {
-                  newState = {
-                    ...newState,
-                    responsiveCSS: {
-                      ...newState.responsiveCSS,
-                      ...{
-                        [attribute]: {
-                          ...newState.responsiveCSS[attribute],
-                          ...(key === "simple"
-                            ? {
-                                simple: value,
-                              }
-                            : {
-                                ...value,
-                              }),
-                        },
-                      },
-                    },
-                  };
-                }
-
-                if (
-                  key === "Object" &&
-                  attributes[attribute].openTypography &&
-                  attributes[attribute].activeSource === "global" &&
-                  attributes[attribute].globalSource === "none"
-                ) {
-                  newState.responsiveCSS[attribute] = {};
-                }
-              };
-              changedAttributes.forEach((changedAttribute) => {
-                updateCSS(
-                  blockAttributes,
-                  attributes,
-                  (attribute, key, value) => updateState(attribute, key, value),
-                  changedAttribute
-                );
-                this.saveCSS(
-                  newState.responsiveCSS,
-                  newState.nonResponsiveCSS,
-                  "update"
-                );
-              });
-              this.setState({
-                responsiveCSS: newState.responsiveCSS,
-                nonResponsiveCSS: newState.nonResponsiveCSS,
-              });
-            }
-          }
-        }
+      componentDidUpdate() {
+        console.log(
+          this.props.name + " ==>> " + this.props.attributes.uniqueId
+        );
+        this.saveCSS();
       }
 
       copyAttributes = () => {
